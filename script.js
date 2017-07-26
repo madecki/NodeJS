57cd063f347cb608fff7eeb42ae681ce64d4a39b
Radosław Madecki <radoslaw.madecki@gmail.com>
2017-07-26 21:28:43 +0200

    Learning in progress

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const EventEmitter = require("events");
-var emitter = new EventEmitter();
+const fs = require("fs");
+const path = require("path");
 
-emitter.on("message", function (msg) {
-    return console.log("Msz jedną wiadomość: ", msg);
+function file(filename) {
+    return path.join(__dirname, "info", filename);
+}
+
+fs.readFile(file("exclude.txt"), function (err, data) {
+
+    if(err) {
+        console.log('Wystapil blad: ${err.message}.');
+        throw err;
+    }
+
+    console.log(data);
+
 });
-
-emitter.emit("message", "To jest wiadomosc bez polskich znakow.");
